3d70f66b52133941ba9ada5f32d5a2932f83adec
Zahraa Alaiwi <zozoa.jalil@gmail.com>
2026-08-07 22:45:22 +0300

    feat: extend user model with additional fields for email, phone number, role, profile completion, status, and suspension date

--- models/User.js
+++ models/User.js
@@ -13,6 +13,37 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true
     },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+    phoneNumber:{
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    role: {
+      type: String,
+      enum: ['worker', 'business'],
+      required: true
+    },
+    isProfileComplete: {
+      type: Boolean,
+      default: false
+    },
+    status: {
+      type: String,
+      enum: ['active', 'suspended'],
+      default: 'active'
+    },
+    suspendedUntil: {
+      type: Date,
+      default: null
+    }
   },
   { timestamps: true },
 );
