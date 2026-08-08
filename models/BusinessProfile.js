3b95223b4a874d1b839ba3d2ef0a6907ad0a9410
Bushra-11 <bu2004hu@gmail.com>
2026-08-08 16:29:53 +0300

    feat: create BusinessProfile model

---
+++ models/BusinessProfile.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const businessProfileSchema = new mongoose.Schema({
+    owner: {
+        type: ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    industry: {
+        type: String,
+        required: true,
+        enum: [
+            'restaurant',
+            'cafe',
+            'hotel',
+            'catering',
+            'event venue',
+            'wedding',
+            'exhibition & conference',
+            'retail',
+            'supermarket',
+            'other'
+        ],
+        trim: true
+    },
+    imageURL: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    websiteURL: {
+        type: String,
+        trim: true
+    }
+
+}, { timestamps: true })
+
+const BusinessProfile = mongoose.model('BusinessProfile', businessProfileSchema)
+
+module.exports = BusinessProfile
