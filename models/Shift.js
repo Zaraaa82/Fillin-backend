f14abee6cc7a381dd3a33637d8945863c29ba366
Bushra-11 <bu2004hu@gmail.com>
2026-08-08 17:14:33 +0300

    feat: create Shift model

---
+++ models/Shift.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose')
+
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const shiftSchema = new mongoose.Schema({
+    postedBy: {
+        type: ObjectId,
+        ref: 'BusinessProfile',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    requiredSkills: {
+        type: [ObjectId],
+        ref: 'Skill',
+        validate: {
+            validator: (skills) => skills.length >= 1,
+            message: 'At least one skill is required.'
+        }
+    },
+    startTime: {
+        type: Date,
+        required: true
+    },
+    endTime: {
+        type: Date,
+        required: true
+
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    payAmount: {
+        type: Number,
+        required: true,
+        min: 10
+    },
+    capacity: {
+        type: Number,
+        required: true,
+        min: 1
+    },
+    applicationDeadline: {
+        type: Date,
+        required: true
+    },
+    status: {
+    type: String,
+    enum: ['open', 'filled', 'cancelled', 'completed'],
+    required: true,
+    default: 'open'
+},
+
+}, { timestamps: true })
+
+const Shift = mongoose.model('Shift', shiftSchema)
+
+module.exports = Shift
