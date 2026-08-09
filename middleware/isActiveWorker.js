08afa630808388c495d4244423da4a8adee401f6
Bushra-11 <bu2004hu@gmail.com>
2026-08-09 16:36:46 +0300

    feat: add isActiveWorker middleware to validate worker status and reactivation

---
+++ middleware/isActiveWorker.js
@@ -0,0 +1,22 @@
+const { reactivateWorkerIfSuspensionExpired } = require('../services/workerSuspensionService');
+
+async function isActiveWorker(req, res, next) {
+  try {
+    const user = await reactivateWorkerIfSuspensionExpired(req.user._id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found.' });
+    }
+    if (user.status === 'suspended') {
+      return res.status(403).json({
+        message: 'Your account is currently suspended.',
+        suspendedUntil: user.suspendedUntil,
+      });
+    }
+    next();
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}
+
+module.exports = { isActiveWorker };
