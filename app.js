edabe25f1a32ca5040e55ddcdc1539cd88107f10
Zahraa Alaiwi <zozoa.jalil@gmail.com>
2026-08-09 23:25:33 +0300

    feat: add worker profile routes to app

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cors = require('cors')
 // Routes Import
 const authRoutes = require('./routes/auth.routes')
 const skillRoutes = require('./routes/skill.routes')
+const workerProfileController = require('./routes/workerProfile.routes');
 
 
 // Middleware
@@ -24,6 +25,7 @@ app.use(morgan('dev'))
 // Routes
 app.use('/auth',authRoutes)
 app.use('/skills', skillRoutes)
+app.use('/worker-profiles', workerProfileController);
 
 
 
